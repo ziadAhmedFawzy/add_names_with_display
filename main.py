3b438fe0a56a29d4a5b18c8f7a5f856d76092f32
ziadAhmedFawzy <ziadbusiness2000@gmail.com>
2023-12-01 20:01:57 +0200

    change welcome sentence

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # input : name , add to info_name , 
 # display names
 
-print("hello, my name is ziad ahmed fawzy")
+print("hello, user")
 
 num_of_names = input("please enter your number of names : ")
 counter = 0
